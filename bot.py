9fa15f89161b701e64e0e9e377aacf7e6250bbcf
alphabharat109-art <alphabharat109@gmail.com>
2026-05-20 21:41:07 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,6 +1,5 @@
 import os
 import logging
-import asyncio
 
 from telegram import Update
 from telegram.ext import (
@@ -11,24 +10,33 @@ from telegram.ext import (
     filters,
 )
 
+# BOT TOKEN FROM RENDER ENV VARIABLE
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 
+# YOUR TELEGRAM USER ID
 ADMIN_ID = 89247204
 
+# STORAGE CHANNEL ID
 STORAGE_CHANNEL_ID = -1003959246499
 
-logging.basicConfig(level=logging.INFO)
+logging.basicConfig(
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+    level=logging.INFO
+)
 
 
+# START COMMAND
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
     args = context.args
 
+    # IF USER OPENED SHARE LINK
     if args:
 
         try:
             msg_id = int(args[0])
 
+            # COPY FILE FROM STORAGE CHANNEL TO USER
             await context.bot.copy_message(
                 chat_id=update.effective_chat.id,
                 from_chat_id=STORAGE_CHANNEL_ID,
@@ -45,22 +53,27 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
         return
 
     await update.message.reply_text(
-        "Send media to store."
+        "Send me media to store."
     )
 
 
+# SAVE MEDIA
 async def save_media(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
     user_id = update.effective_user.id
 
+    # ONLY ADMIN CAN STORE FILES
     if user_id != ADMIN_ID:
+
         await update.message.reply_text(
-            "Not allowed."
+            "You are not allowed."
         )
+
         return
 
     try:
 
+        # COPY MESSAGE TO STORAGE CHANNEL
         stored_message = await context.bot.copy_message(
             chat_id=STORAGE_CHANNEL_ID,
             from_chat_id=update.effective_chat.id,
@@ -73,6 +86,7 @@ async def save_media(update: Update, context: ContextTypes.DEFAULT_TYPE):
             await context.bot.get_me()
         ).username
 
+        # GENERATE SHARE LINK
         share_link = (
             f"https://t.me/{bot_username}?start={msg_id}"
         )
@@ -82,25 +96,29 @@ async def save_media(update: Update, context: ContextTypes.DEFAULT_TYPE):
         )
 
     except Exception as e:
+
         print(e)
 
         await update.message.reply_text(
-            "Failed."
+            "Failed to store file."
         )
 
 
-async def main():
+def main():
 
+    # CREATE APPLICATION
     app = (
         Application.builder()
         .token(BOT_TOKEN)
         .build()
     )
 
+    # COMMAND HANDLER
     app.add_handler(
         CommandHandler("start", start)
     )
 
+    # MEDIA HANDLER
     app.add_handler(
         MessageHandler(
             filters.ALL & ~filters.COMMAND,
@@ -110,8 +128,11 @@ async def main():
 
     print("Bot running...")
 
-    await app.run_polling()
+    # START BOT
+    app.run_polling(
+        drop_pending_updates=True
+    )
 
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    main()
